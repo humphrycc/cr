3a41dafa26c057d39aeb58687836bd59d511ca0b
humphrycc <humphrycc@gmail.com>
2022-09-08 18:32:06 +0800

    only return 403 for cdn

--- 403-normal.lua
+++ 403-normal.lua
@@ -1,2 +1,8 @@
-set_response_status(403)
-set_response_body("this is 403")
+local r = get_request_header('Baidu-Cdn-Request-Id')
+if r and r ~= '' then
+    set_response_status(403)
+    set_response_body("i am sick")
+else
+    set_response_status(200)
+    set_response_body("i am fine")
+end
